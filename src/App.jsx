16369fbe14071f2b356dfdf57c9c8b882d27f891
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-05-21 01:58:57 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="display-calc" />
+    </div>
+  );
+}
+export default App;
